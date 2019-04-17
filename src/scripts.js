a049fdfae446f6c884dc35b01a876cbcb468ad2e
Shreyansh Nayak <nayakshreyansh14@gmail.com>
2019-04-17 20:25:09 +0530

    Fixed settings button and update legend

--- src/scripts.js
+++ src/scripts.js
@@ -4,3 +4,14 @@ document.getElementById("exit").addEventListener("click", function(e) {
   var window = remote.getCurrentWindow();
   window.close();
 });
+
+function settingsMenu() {
+  var element = document.getElementById("Settings");
+  if (element.classList.contains("sAnim-up") == '1') {
+    element.classList.toggle("sAnim-up", false);
+    element.classList.toggle("sAnim-down", true);
+  } else {
+    element.classList.toggle("sAnim-up", true);
+    element.classList.toggle("sAnim-down", false);
+  }
+}
