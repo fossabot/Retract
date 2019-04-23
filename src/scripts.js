9ff1e42a2af82a28dfa9f357cb74776203918030
Shreyansh Nayak <nayakshreyansh14@gmail.com>
2019-04-23 17:54:16 +0530

    Update to icons

--- src/scripts.js
+++ src/scripts.js
@@ -7,12 +7,28 @@ document.getElementById("exit").addEventListener("click", function(e) {
 });
 
 function settingsMenu() {
-  var element = document.getElementById("Settings");
-  if (element.classList.contains("sAnim-up") == '1') {
-    element.classList.toggle("sAnim-up", false);
-    element.classList.toggle("sAnim-down", true);
+  var setAnim = document.getElementById("Settings");
+  if (setAnim.classList.contains("sAnim-up") == '1') {
+    setAnim.classList.toggle("sAnim-up", false);
+    setAnim.classList.toggle("sAnim-down", true);
   } else {
-    element.classList.toggle("sAnim-up", true);
-    element.classList.toggle("sAnim-down", false);
+    setAnim.classList.toggle("sAnim-up", true);
+    setAnim.classList.toggle("sAnim-down", false);
+  }
+}
+
+function invertMode() {
+  var themeM = document.body.getAttribute("theme");
+  var themeImgs = ["Tag", "whatimg", "headin"];
+  if (themeM == "light") {
+    document.body.setAttribute("theme", "dark");
+    for (var i = 0; i < themeImgs.length; i++) {
+      document.getElementById(themeImgs[i]).setAttribute("theme", "dark");
+    }
+  } else {
+    document.body.setAttribute("theme", "light");
+    for (var i = 0; i < themeImgs.length; i++) {
+      document.getElementById(themeImgs[i]).setAttribute("theme", "light");
+    }
   }
 }
